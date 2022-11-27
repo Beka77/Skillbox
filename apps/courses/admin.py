f654b6bac006987b087e7af0da89dff7cf258d18
Beka77 <bekakasymjanov@gmail.com>
2022-11-27 23:42:51 +0600

    first commit

--- apps/courses/admin.py
+++ apps/courses/admin.py
@@ -3,4 +3,7 @@ from .models import Courses
 from apps.settings.models import Currency
 # Register your models here.
 admin.site.register(Courses)
-admin.site.register(Currency)
+admin.site.register
+
+
+(Currency)
